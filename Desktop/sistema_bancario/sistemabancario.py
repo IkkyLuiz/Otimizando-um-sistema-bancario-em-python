0bbd69aa6dd403e07f742321a3e4fb43a555c767
Luiz Henrique <81529238+IkkyLuiz@users.noreply.github.com>
2023-08-20 13:00:56 -0300

    salvando mudanças e alterações no código

--- Desktop/sistema_bancario/sistemabancario.py
+++ Desktop/sistema_bancario/sistemabancario.py
@@ -13,13 +13,16 @@ def menu():
     => """
     return input(textwrap.dedent(menu))
 
-=> """
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-LIMITE_SAQUES = 3
+def depositar(saldo, valor, extrato, /):
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito:\tR$ {valor:.2f}\n"
+        print("\n=== Depósito realizado com sucesso! ===")
+    else:
+        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+
+    return saldo, extrato
 
 while True:
 
