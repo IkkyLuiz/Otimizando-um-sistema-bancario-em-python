50dd3c657f670046fe20739b283ccb2c6bb26ab2
Luiz Henrique <81529238+IkkyLuiz@users.noreply.github.com>
2023-08-20 12:52:17 -0300

    trocando o menu e fazendo atualizações para acrescentar "nova conta", "listar contas' e mais.

--- Desktop/sistema_bancario/sistemabancario.py
+++ Desktop/sistema_bancario/sistemabancario.py
@@ -1,9 +1,17 @@
-menu = """
+import textwrap
+def menu():
+    menu = """\n
+    ================ MENU ================
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+    [d]\tDepositar
+    [s]\tSacar
+    [e]\tExtrato
+    [nc]\tNova conta
+    [lc]\tListar contas
+    [nu]\tNovo usuário
+    [q]\tSair
+    => """
+    return input(textwrap.dedent(menu))
 
 => """
 
